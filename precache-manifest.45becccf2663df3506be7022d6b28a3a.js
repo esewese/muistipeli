966a7cc14e651e6b729572f6d30a311d0bdbe8d9
Esko Vähämäki <esko@surrogate.tv>
2020-01-06 19:40:58 +0200

    Updates

---
+++ precache-manifest.45becccf2663df3506be7022d6b28a3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d60df2e1ce90dd89edaa4f7d4eb3a771",
+    "url": "/muistipeli/index.html"
+  },
+  {
+    "revision": "b5a6c5b247ea7023c3ad",
+    "url": "/muistipeli/static/css/main.03dea66b.chunk.css"
+  },
+  {
+    "revision": "ad9d79dc9139647f98a9",
+    "url": "/muistipeli/static/js/2.f2ec9e8d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/muistipeli/static/js/2.f2ec9e8d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b5a6c5b247ea7023c3ad",
+    "url": "/muistipeli/static/js/main.623c9be7.chunk.js"
+  },
+  {
+    "revision": "5504f37a1a12785f6bcd",
+    "url": "/muistipeli/static/js/runtime-main.c74aaa1b.js"
+  }
+]);
